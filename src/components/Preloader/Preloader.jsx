3c3908f64302bac6be72add3c0c295a3efbb3130
BaichorovNory <62794751+BaichorovNory@users.noreply.github.com>
2021-03-04 18:37:42 +0300

    my simple spa

---
+++ src/components/Preloader/Preloader.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import gif from './ZKZx.gif';
+import style from './Preloader.module.css'
+
+const Preloader = () => {
+    return <div className={style.text}>Загрузка, пожалуйста подождите...<img src={gif} alt='' className={style.img} /></div>;
+};
+
+export default Preloader;
---
+++ src/works.js
@@ -0,0 +1,12 @@
+import spa1 from './img/screenshots/spa-1.jpg';
+
+export default [
+    {
+        id: 'spa-1',
+        title: 'SPA #1',
+        screenshot: spa1,
+        description: 'Первый проект Demo Social Network(ещё не доведен до конца)',
+        link: 'https://github.com/BaichorovNory/social-network-demo',
+        stack: ['react', 'redux']
+    }
+];
