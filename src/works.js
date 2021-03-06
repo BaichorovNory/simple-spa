337cfb7e1a40b99e212676b18aaf2d6e77b2c02d
BaichorovNory <62794751+BaichorovNory@users.noreply.github.com>
2021-03-06 14:24:31 +0300

    Update works.js

--- src/works.js
+++ src/works.js
@@ -1,12 +1,21 @@
-import spa1 from './img/screenshots/spa-1.jpg';
+import react from './img/screenshots/reactt.jpg';
 
 export default [
     {
         id: 'spa-1',
         title: 'SPA #1',
-        screenshot: spa1,
+        screenshot: react,
         description: 'Первый проект Demo Social Network(ещё не доведен до конца)',
         link: 'https://github.com/BaichorovNory/social-network-demo',
         stack: ['react', 'redux']
+    },
+
+    {
+        id: 'spa-2',
+        title: 'SPA #2',
+        screenshot: react,
+        description: 'Второй проект Search Film',
+        link: 'https://github.com/BaichorovNory/Search-Film',
+        stack: ['react', 'axios']
     }
 ];
